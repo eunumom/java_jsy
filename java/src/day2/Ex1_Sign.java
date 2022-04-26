38dd836acb4caae8c41d92d49fcf2692f8980aa4
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-26 10:46:18 +0900

    자바 2일차: 연산자

---
+++ java/src/day2/Ex1_Sign.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Ex1_Sign {
+
+	public static void main(String[] args) {
+		// 정수형 변수 no1을 선언 후 1로 초기화
+		int no1 = -1;
+		System.out.println(no1);
+		System.out.println(-no1);
+		
+
+	}
+
+}
