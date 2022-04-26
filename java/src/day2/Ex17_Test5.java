cdc339272e8c7245ad9355cdabfd3b12cfead62d
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-26 17:00:40 +0900

    Update Ex17_Test5.java

--- java/src/day2/Ex17_Test5.java
+++ java/src/day2/Ex17_Test5.java
@@ -15,7 +15,19 @@ public class Ex17_Test5 {
 		else if (num % 2 == 0) {System.out.println(num + "은 2의 배수");}
 		else if (num % 3 == 0) {System.out.println(num + "은 3의 배수");}
 		else {System.out.println(num + "은 2, 3, 6의 배수가 아님");}
+		
+		/* 선생님의 닶
+		 * 힌트1: 6의 배수를 먼저 체크 - 이렇게 풀었음
+		 * 힌트2: 2의 배수를 확인할 때 3의 배수가 아닌 수를 확인
+		 * */
 
+		if(num % 2 == 0 && num % 3 != 0) {System.out.println(num + "은 2의 배수");}
+		else if(num % 3 == 0 && num % 2 != 0) {System.out.println(num + "은 3의 배수");}
+		else if(num % 6 == 0) {System.out.println(num + "은 6의 배수");}
+		else {System.out.println(num + "은 2, 3, 6의 배수가 아님");}
+		
+	
+		
 	}
 
 }
