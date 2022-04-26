c0f9adcacf93a1340b39efd5bf8c99d6fe6e9c85
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-26 11:05:59 +0900

    자바 2일차: 비교 연산자

---
+++ java/src/day2/Ex5_Comparison.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class Ex5_Comparison {
+
+	public static void main(String[] args) {
+		/* >= 또는 <= 순서를 지켜서 써야 함
+		 * == 를 = 과 혼동하지 않기!
+		 * 결과 값이 참 또는 거짓임
+		 */
+		int score = 95;
+		System.out.println(score > 95);
+		System.out.println(score >= 95);
+		System.out.println(score < 95);
+		System.out.println(score <= 95);
+		System.out.println(score < 95);
+		System.out.println(score == 95);
+		System.out.println(score != 95);
+		
+
+	}
+
+}
