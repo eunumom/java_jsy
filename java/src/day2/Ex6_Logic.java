d1f5c718b900ee4989dabffd356d244c8c8e7b4a
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-26 12:05:14 +0900

    자바 2일차: 조건선택 연산자

--- java/src/day2/Ex6_Logic.java
+++ java/src/day2/Ex6_Logic.java
@@ -3,6 +3,12 @@ package day2;
 public class Ex6_Logic {
 
 	public static void main(String[] args) {
+		/* 논리 연산 종류
+		 * AND: &&
+		 * OR: ||
+		 * XOR: ^ 같으면 거짓, 다르면 참
+		 * NOT: ! 참이면 거짓, 거짓이면 참 
+		 */
 		
 		//성적이 A학점인가? A학점의 기준은 90점 이상 ~ 100점 이하
 		// score가 90점 이상 && score가 100점 이하
@@ -17,7 +23,6 @@ public class Ex6_Logic {
 		
 		int score1 = 105;
 		System.out.println(score1 + "점은 잘못된 성적인가? " + (score1 < 0 || score1 > 100));
-		
 
 	}
 
