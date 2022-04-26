967f3a6d2f0e023962ead525e374f9c1b5da8979
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-26 12:05:32 +0900

    자바 2일차: 비트 연산자

---
+++ java/src/day2/Ex8_Bitlogic.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Ex8_Bitlogic {
+
+	public static void main(String[] args) {
+		byte no1 = 3;
+		byte no2 = 10;
+		System.out.println(no1 & no2);
+		System.out.println(no1 | no2);
+		System.out.println(no1 ^ no2);
+		System.out.println(~ no1);
+		System.out.println(~ no2);
+		// 비트 논리는 계산 했을 때 특정 값이 나옴
+
+	}
+
+}
