c4a7a7215d1fb1d9a55cc8b9511fc5482e6093e3
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-28 17:04:25 +0900

    자바 4일차: Continue_ for와 while

---
+++ java/src/Ex13_Continue1_For.java
@@ -0,0 +1,30 @@
+
+public class Ex13_Continue1_For {
+
+	public static void main(String[] args) {
+		/* 1~10 짝수 출력하기 (Continue 이용)
+		 * 반복횟수: i는 1부터 10까지 1씩 증가
+		 * 규칙성: i가 홀수면 continue 아니면 i 출력
+		 * */
+
+		for(int i=1; i<=10; i++) {
+			if(i%2 !=0) {
+				continue;
+			}
+			System.out.print(i+" ");
+		}
+		
+		int i =1;
+		while(i<=10) {
+			if(i%2 !=0) {
+				i++;
+				continue;
+			}
+			System.out.println(i);
+			i++;
+		}
+		
+		
+	}
+
+}
