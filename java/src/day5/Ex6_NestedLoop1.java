6daddafa06be650e75f1438eae81b8b1de245667
eunumom <103158711+eunumom@users.noreply.github.com>
2022-04-29 13:52:17 +0900

    자바 5일차: *출력하기

---
+++ java/src/day5/Ex6_NestedLoop1.java
@@ -0,0 +1,24 @@
+package day5;
+
+public class Ex6_NestedLoop1 {
+
+	public static void main(String[] args) {
+		/* *****
+		 * *****
+		 * *****
+		 * * j개 출력하기
+		 * */
+		
+		for(int i=1; i<6; i++) {
+			for(int j = 1; j<6; j++) {
+				System.out.print("*");
+			
+			}
+			//엔터를 잊었음
+			System.out.println(); 
+			
+		}
+
+	}
+
+}
