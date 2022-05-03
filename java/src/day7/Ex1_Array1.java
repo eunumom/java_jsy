39a29c39488b5668de830ab20c8deaa65467ab43
eunumom <103158711+eunumom@users.noreply.github.com>
2022-05-03 17:15:13 +0900

    자바 7일차 : 배열 기본 예제

---
+++ java/src/day7/Ex1_Array1.java
@@ -0,0 +1,19 @@
+package day7;
+
+public class Ex1_Array1 {
+
+	public static void main(String[] args) {
+//		5개짜리 배열 0번지=1... 저장 후 출력
+		
+		int [] arr = new int[5];
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=i+1;
+			System.out.print(arr[i]+" ");
+		}
+		
+		
+		
+		
+	}
+
+}
