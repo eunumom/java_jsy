ede996eb7539b3d1a7b8f0d10fa836a3ae5c1d5e
eunumom <103158711+eunumom@users.noreply.github.com>
2022-05-03 17:16:50 +0900

    자바 7일차 : 다차원 배열 예제

---
+++ java/src/day7/Ex8_Array_multidimensional.java
@@ -0,0 +1,23 @@
+package day7;
+
+public class Ex8_Array_multidimensional {
+
+	public static void main(String[] args) {
+		// 1-1의 국어성적 저장을 위한 배열 선언
+		// 각 반 인원 최대 30명
+		int score1 [] = new int [30];
+		
+		// 1학년 전체 학생의 국어성적 저장을 위한 배열 선언
+		// 각 학년은 최대 10반
+		int score2 [] = new int [30*10];
+		int score3 [][] = new int [10][30];
+		score1[10] = 100; // 1반 11번
+		score2[201] = 100; // 7반 22번
+		score3[6][21] = 100; // 7반 22번
+		
+		//1~3학년 전제
+		int score4[][][] = new int [3][10][30];
+		
+	}
+
+}
