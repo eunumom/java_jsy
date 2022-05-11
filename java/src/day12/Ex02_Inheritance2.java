fb1a4b6cf3d6005bdc8b62d70280f519f15da756
eunumom <103158711+eunumom@users.noreply.github.com>
2022-05-11 17:10:59 +0900

    자바 12일차: 상속 _ 기본 속성

---
+++ java/src/day12/Ex02_Inheritance2.java
@@ -0,0 +1,22 @@
+package day12;
+
+public class Ex02_Inheritance2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class A{
+	public void print() {
+		System.out.println("Hello");
+	}
+}
+class B{
+	public void print() {
+		System.out.println("Hi");
+	}
+}
+//class C extends A, B{
+//}							동시상속 안됨
