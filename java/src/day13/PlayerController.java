afec15f35879a518b67552d7ba98934d445c6cf0
eunumom <103158711+eunumom@users.noreply.github.com>
2022-05-12 17:29:57 +0900

    자바 13일차 : 인터페이스 예제 - 상속 예제

---
+++ java/src/day13/PlayerController.java
@@ -0,0 +1,35 @@
+package day13;
+
+public class PlayerController implements Controller, Player {
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prev() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
